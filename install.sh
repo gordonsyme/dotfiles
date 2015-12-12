769aeaeac91f9314c063dd2a18ba33b451821eb6
Gordon Syme <gordon@actinium.local>
2015-12-12 13:52:38 +0000

    Make repeated install work
    Add more packages

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ export ROOT=$(pwd)
 export DOTFILES_ROOT=$(pwd)/dotfiles
 export INCLUDES_ROOT=$(pwd)/include
 
+source scripts/git
+source scripts/homebrew
+source scripts/bash
 source scripts/vim
 source scripts/iterm2
-source scripts/bash
